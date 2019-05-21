a3510305001e62015e526ba6c52421cdb5045b03
yubotao <yubtwy@163.com>
2019-05-21 17:47:12 +0800

    Create PositionHelper.java

---
+++ PositionHelper.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Auther: yubt
+ * @Description: 向列表中插入数据并自动排序，保证不会出现两条记录的position相同；默认原始列表已经按照position进行了排序
+ * @Date: Created in 17:10 2019/5/21
+ * @Modified By:
+ */
+public class PositionHelper {
+
+    public static void main(String[] args) {
+        List<T> original = new ArrayList<>();
+        original.add(new T("One", 1));
+        original.add(new T("Two", 2));
+        original.add(new T("Three", 3));
+        original.add(new T("Four", 4));
+        original.add(new T("Five", 5));
+
+        System.out.println(original);
+
+        T newT = new T("Six", 2);
+        autoPosition(newT, original);
+        System.out.println(original);
+    }
+
+    public static void autoPosition(T t, List<T> tList){
+        // 找到该条数据处于队列中的哪个位置
+        int index = 0;
+        for (T peerT : tList){
+            // 新增的条目与队列中的原有数据位置相同
+            if (t.getPosition() == peerT.getPosition()){
+                // 修改原有数据的位置
+                peerT.setPosition(t.getPosition() + 1);
+                // 存储操作
+                tList.add(index++, t);
+                break;
+            }
+            index++;
+        }
+        int size = tList.size();
+        // 比对到倒数第二个元素，如果相同，最后一个元素会修改值
+        for (int i = index; i < size - 1; i++){
+            // 如果检测到队列中不再存在相同位置的条目，则退出循环，完成所有操作
+            if(!positionPlus(tList.get(i), tList.get(i+1))) break;
+        }
+    }
+
+    private static boolean positionPlus(T t, T nextT){
+        boolean flag = false;
+        if (t.getPosition() == nextT.getPosition()){
+            nextT.setPosition(t.getPosition() + 1);
+            flag = true;
+        }
+        return flag;
+    }
+
+}
+
+class T {
+    String name;
+    int position;
+
+    T(String name, int position){
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "T{" +
+                "name='" + name + '\'' +
+                ", position=" + position +
+                '}';
+    }
+}
